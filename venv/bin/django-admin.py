50ec4609f7f28ec93860f15eb5f0fbb6993f89c7
Kara Carpenter <kara@teachley.com>
2016-04-12 09:21:31 -0400

    My Django Girls app, first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/KaraCarpenter/kara/Python_practice/projects/djangogirls/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
